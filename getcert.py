2014d7d1ae58cd7c4aa15036795f944518289e34
malduhaymi <33746501+malduhaymi@users.noreply.github.com>
2020-03-26 22:01:27 +0300

    Create getcert.py
      V1

---
+++ getcert.py
@@ -0,0 +1,22 @@
+import requests
+
+f = open("C:\\ips.txt", "r")
+for ip in f:
+    
+      try:
+        #getting the IP 
+        newIP=ip.strip()
+        #HTTPS request 
+        requests.get("https://"+newIP)
+      except requests.exceptions.SSLError:
+            try:
+                import ssl
+                import OpenSSL
+                cert = ssl.get_server_certificate((newIP, 443))
+                #getting the cert info
+                cert = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert)
+                #print IP & issued to  
+                print x+" || "+cert.get_subject().get_components()[-1][1]
+            except Exception,e:
+                print e
+
